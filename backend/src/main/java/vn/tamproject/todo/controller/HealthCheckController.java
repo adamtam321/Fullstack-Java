8ae25d1d165180399a481d7b47bc2e7ecf52595d
AdamTam <minhtam.1198@gmail.com>
2026-05-19 01:16:57 +0900

    Fix health check endpoint for UptimeRobot with GET and HEAD support

--- backend/src/main/java/vn/tamproject/todo/controller/HealthCheckController.java
+++ backend/src/main/java/vn/tamproject/todo/controller/HealthCheckController.java
@@ -1,8 +1,8 @@
 package vn.tamproject.todo.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.tamproject.todo.repository.UserRepository;
@@ -17,7 +17,7 @@ public class HealthCheckController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/health-check")
+    @RequestMapping(value = "/health-check", method = {RequestMethod.GET, RequestMethod.HEAD})
     public ResponseEntity<String> healthCheck() {
         // Perform a lightweight database operation to keep the connection/database active
         userRepository.count();
